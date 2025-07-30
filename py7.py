4b6509eb2adc266f68e1c35aa923f7ea61c42acd
jayapriya45 <j64323768@gmail.com>
2025-07-30 16:05:53 +0530

    py7

---
+++ py7.py
@@ -0,0 +1,8 @@
+print("Circle program")
+print("--------------")
+r=int(input("Enter the R value:"))
+print("Result")
+perimeter=2*22/7*r
+print("perameter of circle:",perimeter)
+area=22/7*r*r
+print("area of circle:",area)
