f37cadc0e926633805352a41ea2a68806747bc84
jayapriya45 <j64323768@gmail.com>
2025-07-30 15:51:31 +0530

    py6

---
+++ py6.py
@@ -0,0 +1,15 @@
+print("Square of rectangle")
+print("------------------")
+a=int(input("Enter the A value:"))
+b=int(input("Enter the B value:"))
+l=int(input("Enter the L value:"))
+print("Result")
+perimeter=4 *a
+print("perimeter of square:",perimeter)
+area=a*a
+print("area of square:",area)
+rectangle=2*l+b
+print("Perimeter of rectangle:",rectangle)
+rec=l*b
+print("Area of rectangle:",rec)
+      
