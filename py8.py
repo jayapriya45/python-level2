e41175a03dba33baaeb1124cdb4d7f9a5a1e96b9
jayapriya45 <j64323768@gmail.com>
2025-07-31 10:52:42 +0530

    py8

---
+++ py8.py
@@ -0,0 +1,11 @@
+print("Triangle program")
+print("----------------")
+a=int(input("Enter the A value:"))
+b=int(input("Enter the B value:"))
+c=int(input("Enter the C value:"))
+h=int(input("Enter the H value:"))
+print("Resul")
+perimeter=a+b+c
+print("perimeter of triangle:",perimeter)
+area=1/2*b*h
+print("Area of triangle:",area)
